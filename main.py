1625b422a1f5f0fc436c22d36d3d72d232d1c40c
Goneiross <guillaume.julien.pellerin@gmail.com>
2020-07-11 07:35:20 +0200

    Rework everything

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 import PyPDF4
 import slate3k as slate 
+import os
+import logging
 
-documentName = 'document.pdf'
+logging.propagate = False 
+logging.getLogger().setLevel(logging.ERROR)
 
-with open(documentName, 'rb') as f:
-    extracted_text = slate.PDF(f)
-with open("names.txt") as n:
-    names = [name.rstrip() for name in n]
-pageNumber = len(extracted_text)
+def findWords(documentName) :
+    with open(documentName, 'rb') as f:
+        extracted_text = slate.PDF(f)
+    with open("names.txt") as n:
+        names = [name.rstrip() for name in n]
+    pageNumber = len(extracted_text)
+    print ("Document :", documentName)
+    print ("Nombre de pages :", pageNumber)
+    print("-------------------------------------------------")
 
-print ("Document :", documentName)
-print ("Nombre de pages :", pageNumber)
-print("-------------------------------------------------")
+    for name in names :
+        pages = []
+        for pageIndex in range (0, len(extracted_text)) :
+            if (extracted_text[pageIndex].find(name) != -1) :
+                pages.append(pageIndex + 1)
+        if (pages != []) :
+            print ("Mot trouvé :", name)
+            print (pages)
+    f.close()
+    n.close()
 
-for name in names :
-    print ("Mot recherché :", name)
-    pages = []
-    for pageIndex in range (0, len(extracted_text)) :
-        if (extracted_text[pageIndex].find(name) != -1) :
-            pages.append(pageIndex + 1)
-    print (pages)
-f.close()
-n.close()
+def main():
+    documentNames = []
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            if file.endswith('.pdf'):
+                print(file)
+                documentNames.append(file)
+    for documentName in documentNames :
+        print ("Document :", documentName)
+        findWords(documentName)
+
+main()
